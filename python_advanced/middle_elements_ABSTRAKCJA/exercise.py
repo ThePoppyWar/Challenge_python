8f10557c4271424ca2fc5f8867486504e119a985
anna <anna.jablonska18@gmail.com>
2022-06-04 17:36:58 +0200

    one part

--- python_advanced/middle_elements_ABSTRAKCJA/exercise.py
+++ python_advanced/middle_elements_ABSTRAKCJA/exercise.py
@@ -1,4 +1,32 @@
 def middle_elements(sequences):
-    len_sequences = len(sequences)
-    pass
+    result = []
+    for sequences in sequences:
+        if len(sequences) == 0:
+            continue
+        ind = len(sequences) // 2
+        result.append(sequences[ind])
+    return result
 
+
+print(middle_elements(
+    [[6, 7, 8, 9, 10], ["Kto", "to", "taki?"], [], ["sześć", "siedem", "osiem", "dziewięć"], [8, "to", "osiem"]]))
+
+
+def middle_elements_2(sequences):
+    if len(sequences) == 0:
+        return "List is empty"
+    else:
+        return sequences[int(len(sequences) / 2)]
+
+
+lista1 = [6, 7, 8, 9, 10]
+lista2 = ["Kto", "to", "taki?"]
+lista3 = []
+lista4 = ["sześć", "siedem", "osiem", "dziewięć"]
+lista5 = [8, "to", "osiem"]
+
+print(middle_elements_2(lista1))
+print(middle_elements_2(lista2))
+print(middle_elements_2(lista3))
+print(middle_elements_2(lista4))
+print(middle_elements_2(lista5))
