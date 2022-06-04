f1af8739cded15abfa1e32ae18597ea941eff661
anna <anna.jablonska18@gmail.com>
2022-06-04 18:00:08 +0200

    two part

--- python_advanced/middle_elements_ABSTRAKCJA/exercise.py
+++ python_advanced/middle_elements_ABSTRAKCJA/exercise.py
@@ -1,3 +1,6 @@
+from math import ceil
+
+
 def middle_elements(sequences):
     result = []
     for sequences in sequences:
@@ -30,3 +33,27 @@ print(middle_elements_2(lista2))
 print(middle_elements_2(lista3))
 print(middle_elements_2(lista4))
 print(middle_elements_2(lista5))
+
+class SequeneceOfNumbers:
+    def __init__(self, start:int, stop:int, step: int):
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __len__(self):
+        result = ceil((self.stop - self.start) / self.step)
+        return result
+
+    def __getitem__(self, index):
+        if (index >= 0) and index < len(self):
+            return self.start + index * self.step
+        else:
+            return "IndexError: 'Negative indexes are not supported, sorry!'"
+
+print("========================================")
+nums = SequeneceOfNumbers(14, 46, 4)
+print(len(nums))
+print("========================================")
+print(nums[8])
+print("========================================")
+print(nums[2])
