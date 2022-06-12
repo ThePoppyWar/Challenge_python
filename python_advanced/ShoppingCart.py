d3633c592233d79135e6e4a0cd59c7624917df8e
anna <anna.jablonska18@gmail.com>
2022-06-12 13:02:48 +0200

    Shopping_finish

--- python_advanced/ShoppingCart.py
+++ python_advanced/ShoppingCart.py
@@ -36,15 +36,35 @@ class ShoppingCart:
             else:
                 self.quantities.update({product.id: new_quantity})
 
+    # def get_receipt(self):
+    #     receipt = str()
+    #     total = 0
+    #     for product_id, product in self.products.items():
+    #         receipt += f'{product.name} - amount: {self.quantities.get(product_id)},'\
+    #                    f' price: {product.price} zł, total: {product.price * self.quantities.get(product_id)} \n'
+    #         total += product.price * self.quantities.get(product_id)
+    #     receipt += f'SUMA: {total}zł'
+    #     return receipt
+
+#     Zmodyfikowany get_receipt o rabat 30% za cene za dany produkt
+
     def get_receipt(self):
         receipt = str()
         total = 0
         for product_id, product in self.products.items():
-            receipt += f'{product.name} - amount: {self.quantities.get(product_id)},'\
-                       f' price: {product.price} zł, total: {product.price * self.quantities.get(product_id)} \n'
-            total += product.price * self.quantities.get(product_id)
-        receipt += f'SUMA: {total}zł'
+            if self.quantities.get(product_id) < 3:
+                receipt += f'{product.name} - amount: {self.quantities.get(product_id)},' \
+                           f' price: {product.price} zł, total: {product.price * self.quantities.get(product_id)} \n'
+                total += product.price * self.quantities.get(product_id)
+            else:
+                receipt += f'{product.name} - amount: {self.quantities.get(product_id)},' \
+                           f' price: {product.price}zł' \
+                           f' Suma: {product.price * self.quantities.get(product_id) * 0.7}\n'
+                total += product.price * self.quantities.get(product_id) * 0.7
+        receipt += f'Suma: {round(total, 2)}zł'
         return receipt
+
+
 
 bread = Product('Bread', 2.70)
 ham = Product('Ham', 8.40)
@@ -73,7 +93,7 @@ cart.change_product_quantity(pepper, 2)
 print(cart.products)
 print(cart.quantities)
 
-cart.remove_product(bread)
+# cart.remove_product(bread)
 
 print(cart.products)
 print(cart.quantities)
