8b1778d90c535e4aa77ab80d1c8e25d878cc3d74
anna <anna.jablonska18@gmail.com>
2022-06-15 21:56:01 +0200

    ost_comit_na_dzisiaj

--- python_beginning/tupla_and_list.py
+++ python_beginning/tupla_and_list.py
@@ -38,7 +38,55 @@ print("++++++++++++++++++++++++++++++++++")
 
 lista_lista = [11, 22, 33, 56, 4, 55, 23, 45, 5, 12, 25]
 s= "a vdvafvafv arvadf45v a5dv5a4fv"
-lista_s = list(s)
+
 
 print(lista_lista[0:5]) #or lista_lista[:5]
-print(lista_s[-1:0:2])
+print(s[-1::-2])
+print("++++++++++++++++++++++++++++++++++")
+
+a = "python.py"
+b = "python_not.txt"
+
+def check_python(name):
+    if name[0:6] == "python" and name[-3:] == ".py":
+        return True
+    else:
+        return False
+
+print(check_python(a))
+print(check_python(b))
+
+print("++++++++++++++++++++++++++++++++++")
+imiona = ["Stasia", "Czesiu", "Krysia", "Mysia", "Witold"]
+
+num = 1
+for imie in imiona:
+    print(num, imie)
+    num += 1
+
+# or
+print("--------------------------------------------")
+for num, imie in enumerate(imiona, 1):
+    print(num, imie)
+
+print("--------------------------------------------")
+
+liczby = [4, 5, 7, -3, 2, 8, -10, 15]
+
+# print("pierwszy sposób")
+# liczby = sorted(liczby)
+# print(liczby[-1] - liczby[0])
+
+# print("drugi sposób")
+# najmniejsza= liczby[0]
+# najwieksza = liczby[0]
+#
+# for liczb in liczby:
+#     if liczb < najmniejsza:
+#         najmniejsza = liczb
+#     elif liczb > najwieksza:
+#         najwieksza = liczb
+# print(najwieksza - najmniejsza)
+
+print("trzeci sposób")
+print(max(liczby) - min(liczby))
