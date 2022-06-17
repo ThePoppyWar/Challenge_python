500ccad6601b649783e21d5773a0f703af1b6175
anna <anna.jablonska18@gmail.com>
2022-06-17 10:41:09 +0200

    loop

---
+++ python_beginning/Loop.py
@@ -0,0 +1,17 @@
+import random
+
+
+A = [random.randint(0, 100) for i in range(5)]
+B = [random.randint(0, 100) for i in range(5)]
+H = [random.randint(0, 100) for i in range(5)]
+
+max_v = 0
+licznik = 0
+for a in A:
+    for b in B:
+        for h in H:
+            licznik += 1
+            if a * b * h > max_v:
+                max_v = a * b * h
+print(max_v)
+print(licznik)
