11b7c586c70d4c1d3c76a0f806227d8b33cf56b8
anna <anna.jablonska18@gmail.com>
2022-06-17 19:16:44 +0200

    generator2

--- python_beginning/generator.py
+++ python_beginning/generator.py
@@ -2,6 +2,7 @@ def return_even_number():
     for number in range(2, 20, 2):
         yield number
 
+
 object_generator = return_even_number()
 # print(next(object_generator))
 # print(next(object_generator))
@@ -9,6 +10,11 @@ object_generator = return_even_number()
 # for i in range(10):
 #     print(next(object_generator))
 
-tree = ("*" * n for n in range(5))
-for i in range(5):
-    print(next(tree))
+# tree = ("*" * n for n in range(5))
+# for i in range(5):
+#     print(next(tree))
+
+generator_5 = (n * 5 for n in range(1, 11))
+
+for i in range(11):
+    print(next(generator_5))
