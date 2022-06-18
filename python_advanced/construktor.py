9a84abec701271c70b20c1605de838c893b30b2c
anna <anna.jablonska18@gmail.com>
2022-06-18 08:14:31 +0200

    constructor2

--- python_advanced/construktor.py
+++ python_advanced/construktor.py
@@ -3,10 +3,15 @@ class Dog:
         self.name = name
         self.breed = breed
 
+    def __str__(self):
+        return f"Dog of breed {self.breed} has a name {self.name}"
+
 dos_1 = Dog("Czarus", "Haski")
 dos_2 = Dog("Oczek", "ratlerek")
 dos_3 = Dog("Kiler", "doberman")
 
+print(dos_1)
+print("_____________________________")
 print(dos_1.name)
 print(dos_2.breed)
 print(dos_3.name, dos_3.breed)
