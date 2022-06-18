7b6305b5f27954cc011433faa177da10b68d2615
anna <anna.jablonska18@gmail.com>
2022-06-18 09:46:55 +0200

    frizz2

--- python_beginning/Fizz.py
+++ python_beginning/Fizz.py
@@ -1,15 +1,25 @@
-
 def fizzbuzz(n):
-    for i in range(1, n+1):
-        if i % 15 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(str(i))
-
+    for i in range(1, n + 1):
+        result = ""
+        if i % 3 == 0:
+            result += "Fizz"
+        if i % 5 == 0:
+            result += "Buzz"
+        if i % 3 != 0 and i % 5 != 0:
+            result = str(i)
+        print(result)
 
 print(fizzbuzz(30))
 
+# def fizzbuzz(n):
+#     for i in range(1, n+1):
+#         if i % 15 == 0:
+#             print("FizzBuzz")
+#         elif i % 3 == 0:
+#             print("Fizz")
+#         elif i % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(str(i))
+#
+# print(fizzbuzz(30))
