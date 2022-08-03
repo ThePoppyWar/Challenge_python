283838afa1444274dbbd93dfb8921de8056222f5
anna <anna.jablonska18@gmail.com>
2022-08-03 20:30:03 +0200

    kwarc2

--- python_beginning/args_kwargs.py
+++ python_beginning/args_kwargs.py
@@ -82,3 +82,13 @@ number = (10, 40, 30)
 sample3(*number, **books)
 print("-----------------------------------------------------------")
 
+
+def stick(*args):
+    args = [arg for arg in args if isinstance(arg, str)]
+    result = ','.join(args)
+    return result
+
+
+print(stick('sport', 'summer'))
+print(stick(3, 5, 7))
+print(stick(False, 'time', True, 'workout', [], 'gym'))
